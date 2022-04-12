816fd3d20fd52500f0e231c5f23b0cdafaf41759
Jordi2233 <User123.Name456@protonmail.com>
2022-04-12 12:00:46 +0200

    Delete page added

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const ejs = require("ejs");
 const https = require("https");
 const _ = require("lodash");
 const mongoose = require("mongoose");
+const {
+    stringify
+} = require("querystring");
 
 const homeStartingContent = "I'm baby yuccie gluten-free vice, meditation swag kickstarter kogi pug fanny pack DIY. Slow-carb snackwave tacos kale chips bicycle rights squid. IPhone leggings venmo meggings snackwave four loko bushwick. PBR&B hammock gluten-free selvage, copper mug kickstarter quinoa af sartorial trust fund banjo banh mi actually. Photo booth beard microdosing flannel portland. Kogi keffiyeh crucifix messenger bag af readymade poke food truck, tumeric raw denim typewriter kinfolk prism subway tile.";
 const aboutContent = "Bushwick migas actually meditation tote bag direct trade. Ethical ramps hashtag sartorial taiyaki keytar adaptogen everyday carry la croix raclette. Poke shaman chartreuse cardigan tote bag fam. Glossier hammock fanny pack selvage, truffaut four loko godard flexitarian brooklyn artisan. Cred blue bottle pour-over YOLO drinking vinegar health goth keffiyeh fashion axe schlitz you probably haven't heard of them hexagon. Trust fund gochujang hammock, fixie sriracha hashtag banjo green juice +1.";
@@ -46,7 +49,7 @@ app.use(express.static("public"));
 // *****************************************************************************
 // Get section
 
-app.get("/", function (req, res) {
+app.get("/", (req, res) => {
 
     Post.find({}, (err, foundPosts) => {
         res.render("home", {
@@ -58,23 +61,32 @@ app.get("/", function (req, res) {
 
 });
 
-app.get("/about", function (req, res) {
+app.get("/about", (req, res) => {
     res.render("about", {
         aboutContent: aboutContent
     });
 });
 
-app.get("/contact", function (req, res) {
+app.get("/contact", (req, res) => {
     res.render("contact", {
         contactContent: contactContent
     });
 });
 
-app.get("/compose", function (req, res) {
+app.get("/compose", (req, res) => {
     res.render("compose");
 });
 
-app.get("/posts/:postName", function (req, res) {
+app.get("/delete", (req, res) => {
+    Post.find({}, (err, foundPosts) => {
+        res.render("delete", {
+            homeStartingContent: homeStartingContent,
+            posts: foundPosts
+        });
+    })
+})
+
+app.get("/posts/:postName", (req, res) => {
 
     const reqTitle = req.params.postName;
 
@@ -91,22 +103,12 @@ app.get("/posts/:postName", function (req, res) {
         }
     });
 
-    // posts.forEach(function (post) {
-    //     const storedTitle = _.lowerCase(post.title);
-
-    //     if (reqTitle === storedTitle) {
-    //         res.render("post", {
-    //             postTitle: post.title,
-    //             postContent: post.content
-    //         });
-    //     }
-    // })
 });
 
 // *****************************************************************************
 // Post section
 
-app.post("/compose", function (req, res) {
+app.post("/compose", (req, res) => {
     const postTitle = req.body.newTitle;
     const postContent = req.body.newText;
 
@@ -118,6 +120,28 @@ app.post("/compose", function (req, res) {
     post.save();
 
     res.redirect("/");
+});
+
+app.post("/delete", (req, res) => {
+    const postId = req.body.checkbox;
+    // console.log(typeof postId);
+
+    if (typeof postId === "string") {
+        Post.findByIdAndRemove(postId, (err) => {
+            if (!err) {
+                console.log("Successfully deleted checked item.");
+                res.redirect("/");
+            }
+        });
+    } else {
+        Post.deleteMany({_id: {$in: postId}}, (err) => {
+            if (!err) {
+                console.log("Successfully deleted checked item.");
+                res.redirect("/");
+            }
+        });
+    }
+
 });
 
 
